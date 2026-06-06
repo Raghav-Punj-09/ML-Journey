c396d967d60bbe18344eaeb28972cf3c48fc203d
Raghav-Punj-09 <raghavpunj.feb09@gmail.com>
2026-06-06 23:46:43 +0530

    MY COMMIT MESSAGE

---
+++ day1_basics.py
@@ -0,0 +1,11 @@
+"""My First Day of Python"""
+name = "Raghav"
+age = 20
+gpa = 9.4
+print("My name is", name)
+print("My age is", age)
+print("My GPA is", gpa)
+
+print(type(name))
+print(type(age))
+print(type(gpa))
